dfa0e762b5a3fa46085a6ed908ca664d18e0f8ef
NSSireesha <sireesha.narakuchi@gmail.com>
2024-08-03 08:33:35 +0530

    commit from X

---
+++ src/test/java/secondmax.java
@@ -0,0 +1,35 @@
+
+public class secondmax {
+
+    public static void main(String[] args) {
+
+
+        //o(nlogn)-- sorting . Sorting cannot do duplicate elements
+
+
+        Integer arr[] = {10, 13, 10, 16,16};
+        int max= Integer.MIN_VALUE;
+        int secondmax= Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++)
+        {
+            if (arr[i]>max)
+            {
+                secondmax=max;
+                max=arr[i];
+
+            }
+            else if (arr[i]<max && arr[i] >secondmax)
+            {
+                secondmax=arr[i];
+            }
+        }
+        System.out.println(secondmax);
+
+
+
+
+
+    }
+
+}
