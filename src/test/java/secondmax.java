dbe28f796e64b3d42f559e9cfa84cedcfea2f8a6
NSSireesha <sireesha_narakuchi@epam.com>
2024-08-03 16:37:15 +0530

    changes by user2

--- src/test/java/secondmax.java
+++ src/test/java/secondmax.java
@@ -6,6 +6,11 @@ public class secondmax {
 
         //o(nlogn)-- sorting . Sorting cannot do duplicate elements
 
+// changes by user2
+
+        System.out.println("edit1");
+        System.out.println("edit2");
+        System.out.println("edit3");
 
         Integer arr[] = {10, 13, 10, 16,16};
         int max= Integer.MIN_VALUE;
